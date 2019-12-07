b61a4cbe41b78cd7a0a73582dfb075421c7a5c96
Viktor Lazarev <taurus101v@gmail.com>
2019-12-07 21:31:10 +0100

    04 / 01: it gives an answer, but it's wrong

---
+++ aoc-04.ts
@@ -0,0 +1,90 @@
+const input04 = [357253, 892942];
+
+type Tuple6 = [number, number, number, number, number, number]
+
+const initDigits = (): Tuple6 => [0, 0, 0, 0, 0, 0]
+
+class BruteBorce {
+    current: Tuple6 = initDigits()
+    till: Tuple6 = initDigits()
+    possiblePasswords: Array<number> = []
+    leftToCheck: number = 0
+
+    constructor(from: number, till: number) {
+        this.till = this.toDigits(till)
+        this.current = this.toDigits(from)
+        this.leftToCheck = till - from
+    }
+
+    run(): Array<number> {
+        while (this.leftToCheck >= 0) {
+            if (this.valid()) {
+                this.possiblePasswords.push(this.toNumber())
+            }
+            this.next()
+        }
+
+        return this.possiblePasswords
+    }
+
+    toDigits(num: number): Tuple6 {
+        let digits = initDigits()
+        const maxIndex = digits.length - 1
+
+        for (const index in digits) {
+            digits[maxIndex - parseInt(index, 10)] = num % 10
+            num = Math.floor(num / 10)
+        }
+
+        return digits
+    }
+
+    toNumber(): number {
+        let currentNumber = 0
+        for (const index in this.current) {
+            currentNumber += this.current[index]
+                * 10 ** (this.current.length - 1 - parseInt(index, 10))
+        }
+
+        return currentNumber
+    }
+
+    next(): void {
+        const length = this.current.length;
+        this.current[length - 1] += 1
+
+        for (let index in [...Array(length - 1)]) {
+            const revIndex = length - parseInt(index, 10)
+            if (this.current[revIndex] >= 9) {
+                this.current[revIndex] = 0
+                this.current[revIndex - 1] += 1
+            }
+        }
+
+        this.leftToCheck -= 1
+    }
+
+    valid(): boolean {
+        return this.digitsNotDecrease() && this.hasTwoAdjacentDigits()
+    }
+
+    digitsNotDecrease(): boolean {
+        for (let index in [...Array(this.current.length - 1)]) {
+            if (this.current[index] > this.current[parseInt(index, 10) + 1]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    hasTwoAdjacentDigits(): boolean {
+        for (let index in [...Array(this.current.length - 1)]) {
+            if (this.current[index] === this.current[parseInt(index, 10) + 1]) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+console.log(new BruteBorce(input04[0], input04[1]).run())
